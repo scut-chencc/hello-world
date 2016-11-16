ece616398327445f2d5545e85c25196f4d50caa5
ccc <289893722@qq.com>
2016-11-16 15:42:35 +0800

    add

---
+++ 5open.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+typedef struct student
+{
+	 int num;
+	 char name[ 16];
+}std_t;
+int main( int argc,char* argv[ ])
+{
+	 if( argc!=2)
+	 {
+		  printf( " error args!\n" );
+		  exit( -1);
+	 }
+	 int fd;
+	 fd=open( argv[ 1],O_RDWR|O_CREAT,0666);
+	 if( -1==fd)
+	 {
+		  perror(" open"  );
+		  exit( -1);
+	 }
+	 printf( " open success ,fd is %d\n" ,fd);
+	 int ret;
+	 std_t std[ 2];
+	 memset( std,0,sizeof( std));
+	 //write
+	 std[0].num=9001;
+	 strcpy( std[0].name,"zhangsan") ;
+	 std[1].num=9002;
+	 strcpy( std[1].name,"lisi");
+	 ret=write( fd,&std[0],40);
+	 if( -1==ret)
+	 {
+		  perror( " write" );
+		  exit( -1);
+	 }
+	 //read
+	 memset( std,0,sizeof( std));
+	 ret=read( fd,&std[ 0],40);
+	 if( -1==ret)
+	 {
+		  perror( " read" );
+		  exit( -1);
+	 }
+	 int i;
+	 for(i=0;i<2;i++)
+	 {
+		  printf( " the num is %d,the name is %s\n" ,std[ i].num,std[ i].name);
+	 }
+	 ret=close( fd);
+	 if( -1==ret)
+	 {
+		  perror( " close" );
+		  exit( -1);
+	 }
+	 printf( " close success!\n" );
+	 return 0;
+}
