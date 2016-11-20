18dfe2a2f729cbbebfebb5c228ea70fb7e864330
ccc <289893722@qq.com>
2016-11-20 21:41:27 +0800

    add

---
+++ 5fifo/a.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <strings.h>
+
+int main( int argc,char* argv[ ])
+{
+	 if( argc!=2)
+	 {
+		  printf( " error args\n" );
+		  return -1;
+	 }
+	 int fdr;
+	 fdr=open( argv[ 1],O_RDONLY);
+	 if( -1==fdr)
+	 {
+		  perror( " open" );
+		  return -1;
+	 }
+	 printf( " the fdr is %d\n" ,fdr);
+	 char buf[ 128];
+	 bzero( buf,sizeof( buf));
+	 read( fdr,buf,sizeof( buf));
+	 return 0;
+}
