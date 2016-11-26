ab4583009f55d66706c2a192a801dfb005e6fd31
ccc <289893722@qq.com>
2016-11-26 22:45:55 +0800

    change

--- 6execle_open.c
+++ 6execle_open.c
@@ -7,7 +7,9 @@ int main( )
 {
 	 int fd;
 	 char buf[ 128];
-	 printf("getcwd is %s\n" ,getcwd(buf,128 ));
+	 char* buf2;
+	 buf2=getcwd( buf,128);
+	 printf("getcwd is %s,the return of getcwd is %s\n" ,buf,buf2);
 	 fd=open( "logfile" ,O_RDONLY|O_CREAT,0666);
 	 if( fd<0)
 	 {
