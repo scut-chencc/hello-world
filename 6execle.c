0d8b5af4b7f6a3f497d86ce6228c819bae98dcbb
ccc <289893722@qq.com>
2016-11-26 22:48:52 +0800

    change

--- 6execle.c
+++ 6execle.c
@@ -25,7 +25,7 @@ int main()
 		 exit( -1);
 	}
 	execle( "./6execle_open" ,"6execle_open",NULL,environ);
-    //execle是把后面的程序放入代码段，覆盖而已，还是原来的进程，所以要改变执行路径还需自己设置；
+    //execle是把后面的程序放入代码段，覆盖而已，还是原来的进程，传递进去的环境变量并没影响，所以要改变执行路径还需自己设置；
 	//printf("the pwd is %s\n" ,getenv( "PWD" ));
 	//execl( "./add" ,"add" ,"1" ,"2" ,NULL);
 	printf( "i am the main process\n" );//如果子进程创建成功，这句话被子进程覆盖，并没有执行；
