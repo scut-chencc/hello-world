78f794282feaa125592f7c1f8344714d7a2cec59
ccc <289893722@qq.com>
2016-11-27 21:23:13 +0800

    add

---
+++ 6fork_waitpid2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main( )
+{
+	pid_t pid1,pid2;
+	pid1=fork();
+	int status;
+	int ret;
+	if( 0==pid1)
+	{
+		printf( "i am child1,i have start,mypid is %d\n",getpid( ));
+		sleep(10);
+		exit(1);
+	}else{
+		if(!( pid2=fork()))
+		{
+			printf( "i am child2,i have start,mypid is %d\n",getpid( ));
+			sleep(5);
+			exit(2);
+		}else{
+			printf( "i am father,i have start\n");
+			ret=waitpid(pid1, &status,0);
+			//ret=waitpid(pid1, &status,WNOHANG);
+			if( WIFEXITED(status))
+			{
+				printf( "the return of waitpid1 is %d,the exit status of child1 is %d\n",ret,WEXITSTATUS(status) ) ;
+			}else{
+				 printf( "exit error\n" );
+			}
+			ret=waitpid(pid2, &status,0);
+			//ret=waitpid(pid2, &status,WNOHANG);
+			printf( "the return of waitpid2 is %d,the exit status of child2 is %x\n",ret,status);
+			sleep( 10);
+			exit( 0);
+		}
+	} 
+}
