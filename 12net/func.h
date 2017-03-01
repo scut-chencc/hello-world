823702f67d48f6fb113f0b5f4407e46baa98414d
ccc <289893722@qq.com>
2017-03-01 09:38:10 +0800

    add

---
+++ 12net/func.h
@@ -0,0 +1,11 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <unistd.h>
